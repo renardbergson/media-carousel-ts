840d144080b8ae3b9e9da78fe20c92709c99ded7
renardbergson <renardrock@gmail.com>
2024-12-26 16:54:11 -0300

    inserindo classe Slide

---
+++ src/models/Slide.ts
@@ -0,0 +1,25 @@
+class Slide {
+  container;
+  elements;
+  controls;
+  time;
+
+  constructor(
+    container: Element,
+    elements: Element[],
+    controls: Element,
+    time: number = 5000
+  ) {
+    this.container = container;
+    this.elements = elements;
+    this.controls = controls;
+    this.time = time;
+
+    console.log(this.container);
+    console.log(this.elements);
+    console.log(this.controls);
+    console.log(this.time);
+  }
+}
+
+export default Slide;
